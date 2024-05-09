48fed055b132f427e47eff56ef3521a5f3968e19
fhgggtyf <xjc18502795710@outlook.com>
2024-05-09 23:45:33 +0400

    commit

--- Final/Assets/Scripts/StartupMenuScripts/SceneSwitch.cs
+++ Final/Assets/Scripts/StartupMenuScripts/SceneSwitch.cs
@@ -13,4 +13,18 @@ public class SceneSwitch : MonoBehaviour
         SceneManager.LoadSceneAsync(data.currentStage + 1);
         data.currentStage++;
     }
+
+    public void Exit()
+    {
+        // Log message to confirm quitting (useful for debugging)
+        Debug.Log("Application exit requested");
+
+        // Application.Quit() has no effect in the editor or in web builds
+        Application.Quit();
+
+        // To also ensure it stops playing in the editor
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
 }
